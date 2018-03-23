3c10ab53cc5de3ae6f41afdd1387666ee378d3b6
pollockcl <pollock.clee@gmail.com>
2018-03-23 17:46:40 -0600

    added spec for comment model with validations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214181533) do
+ActiveRecord::Schema.define(version: 20180323234137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
