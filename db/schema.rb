704fbfefc2f01b98adb3314ebba0dcbdf45e28df
nergdvnlt <tylerlundgren@yahoo.com>
2018-03-24 09:26:13 -0600

    Added migration to schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180323235349) do
+ActiveRecord::Schema.define(version: 20180324151005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "job_id"
+    t.index ["job_id"], name: "index_comments_on_job_id"
   end
 
   create_table "companies", force: :cascade do |t|
@@ -48,8 +51,12 @@ ActiveRecord::Schema.define(version: 20180323235349) do
     t.datetime "updated_at", null: false
     t.bigint "company_id"
     t.string "city"
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_jobs_on_category_id"
     t.index ["company_id"], name: "index_jobs_on_company_id"
   end
 
+  add_foreign_key "comments", "jobs"
+  add_foreign_key "jobs", "categories"
   add_foreign_key "jobs", "companies"
 end
