6360585eacc28088b9791bb17bc855844e1aa9ab
pollockcl <pollock.clee@gmail.com>
2018-03-24 17:13:48 -0600

    slight changes to make routes work

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
 
   resources :companies do
-    resources :contacts, only: %i[new edit]
+    resources :contacts, only: [:create]
   end
   
   # The priority is based upon order of creation: first created -> highest priority.
