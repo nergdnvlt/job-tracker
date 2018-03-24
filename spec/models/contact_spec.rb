22882f6fabe10261d136bf3ebb6d28ba536f6b6c
pollockcl <pollock.clee@gmail.com>
2018-03-24 09:54:06 -0600

    added tests for company and contact relationships and dependencies

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,33 +1,74 @@
 require 'rails_helper'
 
 describe Contact do
+
+  before(:each) do
+    @company = Company.new(name: 'Parasite Inc.')
+  end
+
   describe 'validations' do
     context 'invalid attributes - name' do
       it 'is invalid without a name' do
-        contact = Contact.new(position: 'Manager', email: 'bobrob@gmail.com')
+        contact = Contact.new(position: 'Manager',
+                              email: 'bobrob@gmail.com',
+                              company: @company)
+
         expect(contact).to be_invalid
       end
     end
 
     context 'invalid attributes - position' do
       it 'is invalid without a position' do
-        contact = Contact.new(name: 'Bob Ross', email: 'bobrob@gmail.com')
+        contact = Contact.new(name: 'Bob Ross',
+                              email: 'bobrob@gmail.com',
+                              company: @company)
+
         expect(contact).to be_invalid
       end
     end
 
     context 'invalid attributes - email' do
       it 'is invalid without a email' do
-        contact = Contact.new(name: 'Bob Ross', position: 'Manager')
+        contact = Contact.new(name: 'Bob Ross',
+                              position: 'Manager',
+                              company: @company)
+
         expect(contact).to be_invalid
       end
     end
 
     context 'valid attributes' do
       it 'is valid with a name, position, email' do
-        contact = Contact.new(name: 'Bob Ross', position: 'Manager', email: 'bobrob@gmail.com')
+        contact = Contact.new(name: 'Bob Ross',
+                              position: 'Manager',
+                              email: 'bobrob@gmail.com',
+                              company: @company)
+
         expect(contact).to be_valid
       end
+    end
+  end
+
+  describe 'relationships' do
+    it 'has a company' do
+      contact = Contact.new(name: 'Bob Ross',
+                            position: 'Manager',
+                            email: 'bobrob@gmail.com',
+                            company: @company)
+
+      expect(contact).to respond_to(:company)
+    end
+  end
+
+  describe 'dependencies' do
+    it 'disappears with company' do
+      contact = Contact.new(name: 'Bob Ross',
+                            position: 'Manager',
+                            email: 'bobrob@gmail.com',
+                            company: @company)
+      @company.destroy
+
+      expect(Contact.all.size).to eq(0)
     end
   end
 end
