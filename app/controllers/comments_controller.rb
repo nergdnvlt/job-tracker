c54b1c9168f32e124db1a52ac3ec59377d2e7f19
nergdvnlt <tylerlundgren@yahoo.com>
2018-03-24 18:11:50 -0600

    Added create functionality to comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,14 @@
 class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
+    @comment.job_id = params[:job_id]
     @comment.save
-    redirect_to job_path(@comment.job)
+    redirect_to job_path(@comment.job_id)
   end
 
   private
 
   def comment_params
-    params.require.permit(:body)
+    params.require(:comment).permit(:content)
   end
 end
