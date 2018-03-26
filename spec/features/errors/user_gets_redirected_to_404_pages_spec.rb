deb90a6aabe26c76eaaee759979909a9b9cd4c8c
pollockcl <pollock.clee@gmail.com>
2018-03-26 14:05:14 -0600

    added tests for 404 pages at the root level

---
+++ spec/features/errors/user_gets_redirected_to_404_pages_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe 'user visits nonexistent page' do
+  context 'invalid url' do
+    it 'should be a 404 page' do
+      visit '/barnacles'
+
+      expect(current_path).to eq('/barnacles')
+      expect(page.status_code).to eq(404)
+    end
+
+    it 'should have the nav bar' do
+      visit '/barnacles'
+
+      expect(page).to have_link('Jobs')
+      expect(page).to have_link('Companies')
+      expect(page).to have_link('Categories')
+      expect(page).to have_link('Home')
+    end
+  end
+end
