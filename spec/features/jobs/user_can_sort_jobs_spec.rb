74db5c50eccb0ce69c99f8eff47d13e6b151f7d1
pollockcl <pollock.clee@gmail.com>
2018-03-26 09:31:12 -0600

    added tests for sorting by interest level

--- spec/features/jobs/user_can_sort_jobs_spec.rb
+++ spec/features/jobs/user_can_sort_jobs_spec.rb
@@ -30,5 +30,38 @@ describe 'the user can view jobs sorted' do
       expect(page).to have_content('z-city')
     end
   end
+
+  context 'by interest' do
+    it 'sorts by highest interest' do
+      company = Company.create!(name: 'Parasites Inc.')
+      Job.create!(title: 'Developer',
+                  level_of_interest: 13,
+                  city: 'Colorado Springs',
+                  company: company)
+      Job.create!(title: 'Developer',
+                  level_of_interest: 70,
+                  city: 'z-city',
+                  company: company)
+      job = Job.create!(title: 'Developer',
+                        level_of_interest: 90,
+                        city: 'Austin',
+                        company: company)
+
+      visit '/jobs?sort=interest'
+      page.first('a', text: 'Developer').click
+
+      expect(current_path).to eq(job_path(job))
+      expect(page).to have_content(job.level_of_interest)
+      expect(page).to have_content(job.city)
+      expect(page).to have_content(job.title)
+
+      visit '/jobs?sort=interest'
+
+      page.all('a', text: 'Developer').last.click
+
+      expect(page).to have_content('Colorado Springs')
+      expect(page).to have_content('13')
+    end
+  end
 end
 
