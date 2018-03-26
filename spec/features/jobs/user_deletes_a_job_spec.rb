c0106de363b5e40144a888b3829b80f62e81711a
nergdvnlt <tylerlundgren@yahoo.com>
2018-03-26 08:39:57 -0600

    TDD for job edit functionality.

---
+++ spec/features/jobs/user_deletes_a_job_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe 'User deletes existing job' do
+  scenario 'a user can delete a job from index' do
+    company = Company.create!(name: 'Parasites Inc.')
+    category = Category.create(name: 'Sanitation')
+    job = company.jobs.create!(title: 'Developer',
+                               level_of_interest: 70,
+                               city: 'Denver',
+                               company: company,
+                               category: category)
+
+    visit jobs_path
+
+    click_link 'Delete'
+
+    expect(page).to_not have_content(job.title)
+  end
+
+  scenario 'a user can delete a job from show' do
+    company = Company.create!(name: 'Parasites Inc.')
+    category = Category.create(name: 'Sanitation')
+    job = company.jobs.create!(title: 'Developer',
+                               level_of_interest: 70,
+                               city: 'Denver',
+                               company: company,
+                               category: category)
+
+    visit job_path(job)
+    click_link 'Delete'
+
+    expect(page).to_not have_content(job.title)
+  end
+end
