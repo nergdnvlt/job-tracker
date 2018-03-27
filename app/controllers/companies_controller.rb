9f69267b5019351edd97f52df5c64a0e6aae74da
pollockcl <pollock.clee@gmail.com>
2018-03-27 17:23:51 -0600

    slight fixes

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,6 @@
 class CompaniesController < ApplicationController
+  before_action :set_company, only: %i[show edit update destroy]
+
   def index
     @companies = Company.all
   end
@@ -18,18 +20,13 @@ class CompaniesController < ApplicationController
   end
 
   def show
-    @company = Company.find(params[:id])
     @contact = Contact.new
   end
 
-  def edit
-    @company = Company.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @company = Company.find(params[:id])
-    @company.update(company_params)
-    if @company.save
+    if @company.update(company_params)
       flash[:success] = "#{@company.name} Updated!"
       redirect_to company_path(@company)
     else
@@ -38,15 +35,17 @@ class CompaniesController < ApplicationController
   end
 
   def destroy
-    company = Company.find(params[:id])
-    company.destroy
+    @company.destroy
 
-    flash[:success] = "#{company.name} was successfully deleted!"
+    flash[:success] = "#{@company.name} was successfully deleted!"
     redirect_to companies_path
   end
 
-
   private
+
+  def set_company
+    @company = Company.find(params[:id])
+  end
 
   def company_params
     params.require(:company).permit(:name, :city)
