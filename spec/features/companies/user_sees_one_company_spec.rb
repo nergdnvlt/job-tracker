e2fa8a2f6d9ee4b2dd23ea459c676fb0bcd853f2
pollockcl <pollock.clee@gmail.com>
2018-03-28 09:16:43 -0600

    wrote tests for the company show page to display jobs and added one for contacts displayment

--- spec/features/companies/user_sees_one_company_spec.rb
+++ spec/features/companies/user_sees_one_company_spec.rb
@@ -3,11 +3,41 @@ require 'rails_helper'
 describe 'User sees one company' do
   scenario 'a user sees a company' do
     company = Company.create!(name: 'ESPN')
-    company.jobs.create!(title: 'Developer', level_of_interest: 90, city: 'Denver')
+    company.jobs.create!(title: 'Developer',
+                         level_of_interest: 90,
+                         city: 'Denver')
 
     visit company_path(company)
 
     expect(current_path).to eq(company_path(company))
     expect(page).to have_content('Contacts')
+    expect(page).to have_content('Jobs')
+  end
+
+  scenario 'user sees all the companys contacts' do
+    company = Company.create!(name: 'ESPN')
+    company.jobs.create!(title: 'Developer',
+                         level_of_interest: 90,
+                         city: 'Denver')
+    contact = company.contacts.create(name: 'Barry Allen',
+                                     position: 'Speedster',
+                                     email: 'ballen@domain.com')
+
+    visit company_path(company)
+
+    expect(page).to have_content(contact.name)
+    expect(page).to have_content(contact.position)
+    expect(page).to have_content(contact.email)
+  end
+
+  scenario 'user sees all the companys jobs' do
+    company = Company.create!(name: 'ESPN')
+    job = company.jobs.create!(title: 'Developer',
+                               level_of_interest: 90,
+                               city: 'Denver')
+
+    visit company_path(company)
+
+    expect(page).to have_content(job.title)
   end
 end
