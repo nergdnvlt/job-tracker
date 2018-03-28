c8db86dca2ab437895d605234f08e3e931faaa76
nergdvnlt <tylerlundgren@yahoo.com>
2018-03-28 11:18:51 -0600

    Small test refactor.

--- spec/features/jobs/user_can_sort_jobs_spec.rb
+++ spec/features/jobs/user_can_sort_jobs_spec.rb
@@ -4,18 +4,22 @@ describe 'the user can view jobs sorted' do
   context 'by city' do
     it 'sorts by city name' do
       company = Company.create!(name: 'Parasites Inc.')
+      category = Category.create!(name: 'Hero')
       Job.create!(title: 'Developer',
-                  level_of_interest: 70,
+                  level_of_interest: 13,
                   city: 'Colorado Springs',
-                  company: company)
+                  company: company,
+                  category: category)
       Job.create!(title: 'Developer',
                   level_of_interest: 70,
                   city: 'z-city',
-                  company: company)
+                  company: company,
+                  category: category)
       job = Job.create!(title: 'Developer',
-                        level_of_interest: 70,
+                        level_of_interest: 90,
                         city: 'Austin',
-                        company: company)
+                        company: company,
+                        category: category)
 
       visit '/jobs?sort=city'
       page.first('a', text: 'Developer').click
@@ -34,18 +38,22 @@ describe 'the user can view jobs sorted' do
   context 'by interest' do
     it 'sorts by highest interest' do
       company = Company.create!(name: 'Parasites Inc.')
+      category = Category.create!(name: 'Hero')
       job1 = Job.create!(title: 'Developer',
                          level_of_interest: 13,
                          city: 'Colorado Springs',
-                         company: company)
+                         company: company,
+                         category: category)
       job2 = Job.create!(title: 'Developer',
                          level_of_interest: 70,
                          city: 'z-city',
-                         company: company)
+                         company: company,
+                         category: category)
       job3 = Job.create!(title: 'Developer',
                          level_of_interest: 90,
                          city: 'Austin',
-                         company: company)
+                         company: company,
+                         category: category)
 
       visit '/jobs?sort=interest'
       page.first('a', text: 'Developer').click
